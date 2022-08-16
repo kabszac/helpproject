24d93de996837370f1ea41cf905ef2072797d2c8
Isaac Kabucho <kabszac@gmail.com>
2022-08-16 18:44:04 +0300

    Made changes to settings.py and added six package

--- help_project/settings.py
+++ help_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jf(!!4x%6lesg)=gf#)_apl%_8jd%l1x^$fm$17ls2o^d$)wm0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
