5cd8c0c29e8a71c1bb5a07cc683fd71fde9be602
Isaac Kabucho <kabszac@gmail.com>
2022-08-17 17:38:12 +0300

    Made changes to settings.py

--- help_project/settings.py
+++ help_project/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://selfhelpproject.herokuapp.com/', 'http://127.0.0.1:8000/']
+ALLOWED_HOSTS = ['https://selfhelpproject.herokuapp.com', 'http://127.0.0.1:8000/']
 
 
 # Application definition
