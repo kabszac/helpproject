5a507dd932cade05998d735453124cf30dfc571b
Isaac Kabucho <kabszac@gmail.com>
2023-03-08 13:46:43 +0300

    made changes to settings and others

--- help_project/settings.py
+++ help_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['selfhelpproject.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['web-production-8f9b.up.railway.app', 'selfhelpproject.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -92,8 +92,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASE_URL = os.getenv("DATABASE_URL")
+db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
 DATABASES['default'].update(db_from_env)
 
 
--- users/views.py
+++ users/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm,  UserUpdateForm, ProfileRegisterForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
-# Create your views here.
 
 def register(request):
     if request.method == 'POST':
@@ -11,29 +10,33 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"Your account has been created you can now log in ")
+            messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
 
-    return render(request, 'users/register.html', {'form':form})
 
 @login_required
 def profile(request):
     if request.method == 'POST':
-        p_form = UserUpdateForm(request.POST, instance = request.user)
-        i_form = ProfileRegisterForm(request.POST, request.FILES, instance = profile)
-        if p_form.is_valid() and i_form.is_valid():
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
             p_form.save()
-            i_form.save()
-            messages.success(request, f"Your profile has been updated")
+            messages.success(request, f'Your account has been updated!')
             return redirect('profile')
+
     else:
-        p_form = UserUpdateForm( instance = request.user)
-        i_form = ProfileRegisterForm( instance = request.user.profile)
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
     context = {
-        'p_form':p_form,
-        'i_form':i_form
+        'u_form': u_form,
+        'p_form': p_form
     }
 
     return render(request, 'users/profile.html', context)
