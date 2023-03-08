94583de15d12ac83eead5b9db6c21410e4c38e40
Isaac Kabucho <kabszac@gmail.com>
2023-03-08 14:07:55 +0300

    Added changes to settings

--- help_project/settings.py
+++ help_project/settings.py
@@ -86,15 +86,21 @@ WSGI_APPLICATION = 'help_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 DATABASE_URL = os.getenv("DATABASE_URL")
-db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
-DATABASES['default'].update(db_from_env)
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+}
+
+# db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+# DATABASES['default'].update(db_from_env)
 
 
 # Password validation
